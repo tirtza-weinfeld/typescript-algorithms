75af793687e9dc40cc4370a55ad76d80d8a1e857
tz-i-git <tzu.22@icloud.com>
2024-04-03 00:46:08 +0300

    networks

--- src/shortest-path/bfs.ts
+++ src/shortest-path/bfs.ts
@@ -20,8 +20,6 @@ export function bfs(graph: Graph, s: string) {
 
 
 
-
-
 export function findShortestPath<V extends Vertex, E extends Edge<V>>(graph: Graph<V, E>, s: string, t: string) {
     const parent = search(graph, s, 'bfs').parent;
     if (!parent) return;
@@ -30,56 +28,9 @@ export function findShortestPath<V extends Vertex, E extends Edge<V>>(graph: Gra
     for (let p = parent[t]; p && p !== s; p = parent[p])
         path = [p, ...path];
 
-
-    // return !path.length ? [] : parent[path[0]] ? [parent[path[0]], ...path, t] : [...path, t];
-
     if (path.length === 1 && parent[t] !== s) return [];
     return parent[path[0]] ? [parent[path[0]], ...path] : path;
 }
-
-
-
-// export function bfs(adj: any, s: any) {
-
-//     const parent = List(null, adj.length);
-//     parent[s] = s;
-//     const level = [[s]];
-
-//     while (0 < level[-1].length) {
-//         level.push([])
-//     }
-//     for (let u of level[-2]) {
-//         for (let v of adj[u]) {
-//             if (parent[v] === null) {
-
-//                 parent[v] = u;
-//                 level[-1].push(v);
-//             }
-//             return parent;
-//         }
-//     }
-
-
-// }
-
-
-
-
-
-
-
-// /**
-//  * search algorithm that always expands the
-//  * **shallowest** node in the frontier
-//  * @param adj 
-//  * @param s 
-//  * @returns 
-//  */
-// export function bfs(adj: any, s: any) {
-
-//     const frontier = [];
-
-// }
 
 
 
